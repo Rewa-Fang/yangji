49f9a504c0748d8dd6fb1a1d44f7b8632eaa2581
Rewa Fang <fangrh0627@163.com>
2018-09-02 18:51:38 +0800

    details page

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 (function () {
-    
+    case_gallery();
 })();
 function signIn() {
     document.getElementById("mask").style.display = "block";
@@ -13,4 +13,16 @@ function closeForm(){
     document.getElementById("mask").style.display = "none";
     document.getElementById("signinForm").style.display = "none";
     document.getElementById("signupForm").style.display = "none";
+}
+
+function case_gallery(){
+    console.log(1111);
+    
+	$('#small_img .small_img_list .bd').delegate('span', 'click', function(){
+		var img=$(this).attr('src');
+		// $('#bigimg_src').attr('src', img).parent().attr('href', img);
+        $('#bigimg_src').attr('src', img);
+        console.log(img);
+		$(this).addClass('on').siblings('span').removeClass('on');
+	});
 }
