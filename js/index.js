193259f4f32114a65c415d793038b04a994d167b
Rewa Fang <fangrh0627@163.com>
2018-09-02 19:16:05 +0800

    details message

--- js/index.js
+++ js/index.js
@@ -16,13 +16,20 @@ function closeForm(){
 }
 
 function case_gallery(){
-    console.log(1111);
     
 	$('#small_img .small_img_list .bd').delegate('span', 'click', function(){
-		var img=$(this).attr('src');
+		var img=$(this).attr('pic');
 		// $('#bigimg_src').attr('src', img).parent().attr('href', img);
         $('#bigimg_src').attr('src', img);
-        console.log(img);
 		$(this).addClass('on').siblings('span').removeClass('on');
 	});
+}
+
+function showMessage(){
+    $('#mask').show();
+    if(localStorage.userInfo){
+        $('#messageForm').show();
+    }else{
+        $('#messageFormNoLogin').show();
+    }
 }
